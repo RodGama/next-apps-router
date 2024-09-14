d3c5b79cec3dbcf1faef17fba16755e20592b667
Rodrigo Carvalho <rodrigocgama@protonmail.com>
2024-09-14 17:41:04 -0300

    inicio styled components

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next'],
   collectCovegare: true,
-  collectCovegareFrom: ['src/**/*.ts(x)?'],
+  collectCovegareFrom: [
+    'src/**/*.ts(x)?',
+    '!src/appp/**',
+    '!src/lib/registry.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulesPaths: ['<rootDir>/src/'],
   transform: {
